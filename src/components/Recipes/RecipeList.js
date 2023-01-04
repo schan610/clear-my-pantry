5dd7fe0a2908ac71e70fe509b67594bc849ef6f8
schan610 <s.chan610@gmail.com>
2023-01-04 15:10:23 -0800

    added condition if recipes are empty

--- src/components/Recipes/RecipeList.js
+++ src/components/Recipes/RecipeList.js
@@ -2,7 +2,9 @@ import classes from "./RecipeList.module.css";
 import RecipeItem from "./RecipeItem";
 
 const RecipeList = (props) => {
-  console.log(props.recipes);
+  if (props.recipes.length === 0) {
+    return <p>No queries found</p>;
+  }
   return (
     <div className={classes["recipe__list"]}>
       {props.recipes.map((rec) => (
